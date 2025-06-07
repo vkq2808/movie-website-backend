85dfc0dc0561d5a5f73d939d9f10d5c128544f83
Quoc <vkq0919309031@gmail.com>
2025-06-07 16:17:51 +0700

    config cors for deployment

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.CORS_ORIGIN ?? '*',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 204,
