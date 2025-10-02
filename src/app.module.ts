6f0641371afcccdf628a841343e7bfa8ec002f47
Quoc <vkq0919309031@gmail.com>
2025-10-02 20:34:41 +0700

    refactor: remove SeederModule and related services, streamline codebase

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,7 +20,7 @@ import {
   AdminModule,
   CloudinaryModule,
   VideoModule,
-  SeederModule,
+  // SeederModule,
 } from '@/modules';
 import { LoggerMiddleware } from './middlewares/logger.middlewares';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -74,7 +74,7 @@ dotenv.config();
     ProductionCompanyModule,
     RecommendationModule,
     AdminModule,
-    SeederModule,
+    // SeederModule,
   ],
   providers: [AppService, TasksService],
   controllers: [AppController],
--- src/modules/seeder/dtos/production-company.dto.ts
+++
@@ -1,9 +0,0 @@
-export type TMDBProductionCompanyDetails = {
-  id: number;
-  description?: string | null;
-  headquarters?: string | null;
-  homepage?: string | null;
-  logo_path?: string | null;
-  name: string;
-  origin_country?: string | null;
-}
