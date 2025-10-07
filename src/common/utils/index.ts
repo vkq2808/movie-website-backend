1f10aa211e7920b46f2f3e4b71f546061a3e6a5d
Quoc <vkq0919309031@gmail.com>
2025-10-07 14:07:04 +0700

    fix: set minimum machines running to 1 in fly.toml and remove unused export in index.ts

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -1,6 +1,5 @@
 import api from './axios.util';
 export * from './puppeteer.util';
 export * from './locale.util';
-export * from '../../modules/seeder/utils/check-vietnamese-text.util'
 
 export { api };
